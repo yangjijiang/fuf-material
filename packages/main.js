3afb37597920ca39b082e2d316d6190608bb94c5
yangjijiang <yangjijiang@sina.cn>
2021-04-19 15:05:44 +0800

    水印组件图片资源

--- packages/main.js
+++
@@ -1,8 +0,0 @@
-import Vue from "vue";
-const App = require(`./${process.env.VUE_APP_NAME}/src`).default
-
-Vue.config.productionTip = false;
-
-new Vue({
-    render: h => h(App)
-}).$mount("#app");
