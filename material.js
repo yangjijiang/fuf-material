f391f2ad77784c697cf53b88fb58584efe078a04
yangjijiang <yangjijiang@sina.cn>
2021-04-23 17:04:56 +0800

    物料图片

--- material.js
+++ material.js
@@ -7,7 +7,9 @@ fs.readdirSync(dirPath).forEach((name) => {
   const filePath = path.join(dirPath, name, 'package.json')
   if (fs.existsSync(filePath)) {
     const p = require(filePath)
-    materials.push({name: name, ...p.material})
+    // const bitmap = fs.readFileSync('assets/watermark/index.jpg');
+    // const base64str = Buffer.from(bitmap, 'binary').toString('base64');
+    materials.push({name: name, desc: p.description, ...p.material})
   }
 })
 
